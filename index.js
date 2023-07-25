3d5639f302e6d35220495abf320845229cf264e9
KatiaProkina <prokina.katia@gmail.com>
2023-07-25 14:50:52 +0300

    1

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const optionMenu = document.querySelector('.select-menu')
 const selectBtn = document.querySelector('.select-btn')
-// const options = document.querySelectorAll('.option')
+const options = document.querySelectorAll('.option')
 const sBtn_text = document.querySelector('.sBtn-text')
 
 const currencyList =  document.querySelector('.options')
@@ -32,9 +32,9 @@ getCurrency()
 
 selectBtn.addEventListener('click', ()=> optionMenu.classList.toggle("active"));
 
-// options.forEach(option =>{
-//     option.addEventListener('click',()=>{
-//         let selectedOption = option.querySelector('.option-text').innerText;
-//         sBtn_text.innerText = selectedOption
-//     })
-// })
+options.forEach(option =>{
+    option.addEventListener('click',()=>{
+        let selectedOption = option.querySelector('.option-text').innerText;
+        sBtn_text.innerText = selectedOption
+    })
+})
